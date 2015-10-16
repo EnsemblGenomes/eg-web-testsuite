22ccdadb5b6599510954a544e51dc31917978b2c
Nick Langridge <nicklangridge@hotmail.com>
2015-10-16 09:49:59 +0100

    Add Bacteria module

---
+++ modules/EnsEMBL/Selenium/Test/Bacteria.pm
@@ -0,0 +1,7 @@
+package EnsEMBL::Selenium::Test::Bacteria;
+use strict;
+use base 'EnsEMBL::Selenium::Test::EGCommon';
+
+__PACKAGE__->set_default('species', 'escherichia_coli_str_k_12_substr_mg1655');
+
+1;
